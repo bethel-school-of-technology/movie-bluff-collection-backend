046123af2af5baf19487f5111ed008baa685372d
ansverner <51131574+ansverner@users.noreply.github.com>
2020-01-24 21:16:14 -0500

    Update associations.js

--- models/rel/associations.js
+++ models/rel/associations.js
@@ -7,33 +7,6 @@ module.exports = function (models) {
         through: models.userMovies,
         foreignKey: 'userId',
     });
-    // models.userMovies.belongsToMany(models.movies,
-    //     {
-    //         through: models.userMovies,
-    //         foreignKey: 'imdbId'
-    //     });
-    // models.movies.hasMany(models.userMovies,
-    //     {
-    //         foreignKey: 'imdbId',
-    //         targetKey: 'imdbId'
-    //     }
-    // ),
-    // models.movies.belongsToMany(models.users,
-    //         {
-    //             through: models.users,
-    //             foreignKey: 'userId'
-    //         });
-    // models.users.hasMany(models.movies,
-    //     {
-    //         foreignKey: 'userId',
-    //         targetKey: 'userId'
-    //     }
-    // )
-
-    //     models.userMovies.belongsTo(models.movies);
-    // models.movies.hasMany(models.userMovies);
-    // models.movies.belongsTo(models.users);
-    // models.users.hasMany(models.movies);
 }
 
 
